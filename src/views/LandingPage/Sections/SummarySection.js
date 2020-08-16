7b7d70b1fe6de27fe10995a7c9f58268acd7459c
Leo Yang <leo19941227@gmail.com>
2020-08-17 01:39:28 +0800

    rename product to summary

--- src/views/LandingPage/Sections/SummarySection.js
+++ src/views/LandingPage/Sections/SummarySection.js
@@ -2,25 +2,17 @@ import React from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 
-// @material-ui/icons
-import Chat from "@material-ui/icons/Chat";
-import VerifiedUser from "@material-ui/icons/VerifiedUser";
-import Fingerprint from "@material-ui/icons/Fingerprint";
 // core components
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
-import InfoArea from "components/InfoArea/InfoArea.js";
 
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 import { Paper } from '@material-ui/core';
 
-import styles from "assets/jss/material-kit-react/views/landingPageSections/productStyle.js";
+import styles from "assets/jss/material-kit-react/views/landingPageSections/summaryStyle.js";
 
 const useStyles = makeStyles(styles);
 
-export default function ProductSection() {
+export default function SummarySection() {
   const classes = useStyles();
   return (
     <div className={classes.section}>
